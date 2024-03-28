c9495ec0c2af028d7357390089f44424018c1c44
Rohithlaa <45815002+Rohithlaa@users.noreply.github.com>
2024-03-28 16:01:14 +0530

    Time: 69 ms (65.50%) | Memory: 54.6 MB (56.06%) - LeetSync

--- 713-subarray-product-less-than-k/subarray-product-less-than-k.js
+++ 713-subarray-product-less-than-k/subarray-product-less-than-k.js
@@ -9,7 +9,7 @@ var numSubarrayProductLessThanK = function(nums, k) {
     let j = 0; // initializing j slider point B
     let product = 1; // we require product of subarray, so initializing product to 1
     let count = 0; // we need to return count, so for calculating count initializing the count 
-    if(k <= 1) return 0; // if k is less than 0, then the product of any subarray is 0 and count is 0
+    if(k <= 1) return 0; // if k is less than 1, then the product of any subarray is 0 and count is 0
     while(j< nums.length){ // we wil loop till slider point B ends i.e still length of array
         product *= nums[j] // calculating the product
         while( product >= k){ // catching the product greater equal 100
